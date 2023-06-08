6ecbc398589ed3800bbcb704d4d68cb68c75d3e4
Beknazar Esenbek <beknazar.esenbek@fiware.org>
2023-06-08 14:31:02 +0200

    tmp 4

--- server.js
+++ server.js
@@ -455,7 +455,7 @@ var renderTemplate = function(req, res, viewName) {
 };
 
 app.get(config.portalPrefix + '/', function(req, res) {
-    renderTemplate(req, res, 'app');
+    renderTemplate(req, res, 'app2');
 });
 
 app.get(config.portalPrefix + '/payment', ensureAuthenticated, function(req, res) {
